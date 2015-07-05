28c0e8a31707dbf1290b7027c079257e1af0a635
Ian <ian@iancmacdonald.com>
2015-07-05 16:16:47 -0700

    Can add a user to the site

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome to the site!"
+      redirect_to "/"
+    else
+      flash[:alert] = "There was a problem creating your account. Please try again."
+      render :new
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ActiveRecord::Base
+
   attr_accessor :password
   validates_confirmation_of :password
+  validates :email, presence: true
+  validates :password, presence: true
   before_save :encrypt_password
 
   def encrypt_password
+# binding.pry
     self.password_salt = BCrypt::Engine.generate_salt
     self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
   end
